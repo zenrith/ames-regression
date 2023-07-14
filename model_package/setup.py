db5d890e957016a7476f90ee6352ea834a2dd18b
Jeffrey <jeffreygx1@gmail.com>
2023-07-14 22:47:03 +0800

    packaging related

---
+++ model_package/setup.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+NAME = 'ames_model'
+DESCRIPTION = "Trained regression model for the ames dataset"
+REQUIRES_PYTHON = ">=3.11.0"
+
+about = {}
+ROOT_DIR = Path(__file__).resolve().parent
+REQUIREMENTS_DIR = ROOT_DIR / 'requirements'
+PACKAGE_DIR = ROOT_DIR / 'ames_model'
+with open(PACKAGE_DIR / "VERSION") as f:
+    _version = f.read().strip()
+    about["__version__"] = _version
+
+def list_reqs(fname="prod.txt"):
+    with open(REQUIREMENTS_DIR / fname) as fd:
+        return fd.read().splitlines()
+
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=("tests",)),
+    package_data={"ames_model": ["VERSION"]},
+    install_requires=list_reqs(),
+    include_package_data=True,
+    license="BSD-3",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],    
+)
